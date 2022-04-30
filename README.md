94e82043228a2e6ba32e09692b7c067fddc3756b
nullenvk <nullenvk@protonmail.com>
2022-04-30 19:53:41 +0200

    svmon.sh

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Runit scripts
-This is my personal collection of various runit scripts. 
+This is my personal collection of various runit init scripts and tools. 
 
-## Script Descriptions
+## Init scripts
 * hddmount - mount encrypted HDDs as soon as they become available
 * runsvdir-user - permits an user to run runit scripts in their home directory
 * irc - an autostarting irssi session (use it as an user service)
+
+## Utils
+* svmon.sh - Real-time monitor of services running in user's svdir
---
+++ svmon.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+UPDATE_FREQ=1
+SVDIR=$HOME/.runit/service
+
+print_service() {
+    case $(<$1/supervise/stat) in
+        "run") echo -ne "[\e[32mRUN\e[0m]" ;;
+        *) echo -ne "[\e[31m???\e[0m]" ;;
+    esac
+
+    echo " $(basename $1)"
+}
+
+print_all() {
+    for sv in $SVDIR/*; do
+        print_service $sv
+    done
+}
+
+draw() {
+    echo "$(print_all)" | column
+    exit 0
+}
+
+showhelp() {
+    echo "Help here"
+    exit 1
+}
+
+# Argument parsing
+OPTSTRING=":hs"
+while getopts ${OPTSTRING} arg; do
+    case "${arg}" in
+        h) showhelp ;;
+        s) draw ;;
+        ?)
+            echo "Invalid option: -${OPTARG}"
+            showhelp
+            ;;
+    esac
+done
+
+# Main loop
+exec watch -c -t -n $UPDATE_FREQ -x bash -c "./$0 -s 2> /dev/null"
